342e84c643b14b3b2df2ed26fb02f401efc0b45b
Joel Buter <jtbuter@gmail.com>
2023-04-03 14:13:15 +0200

    gebruikt nu VecEnv, maar deze implementatie lijkt een stuk trager te zijn

--- stable-baselines-practice/Q.py
+++ stable-baselines-practice/Q.py
@@ -2,7 +2,6 @@ import traceback
 import warnings
 import time
 import sys
-from copy import deepcopy
 from typing import Any, Dict, List, Optional, Tuple, Type, TypeVar, Union
 
 import numpy as np
@@ -22,134 +21,105 @@ from stable_baselines3.common.utils import get_linear_fn, get_parameters_by_name
 from stable_baselines3.common.vec_env import VecEnv
 from QPolicy import QPolicy
 
-SelfQ = TypeVar("SelfQ", bound="Q")
-
 class Q(BaseAlgorithm):
+    def _setup_model(self) -> None:
+        pass
+
     policy_aliases: Dict[str, Type[BasePolicy]] = {
         "MlpPolicy": BasePolicy
     }
 
-    def __init__(self, env, learning_rate):
+    def __init__(self, env, learning_rate, gamma):
         super().__init__('MlpPolicy', env, learning_rate)
 
-        self.rollout = None
-        self.Q_table = None
-        self.gamma = 0.99
+        # self.env = env
 
-        self._setup_model()
-        
-    def _setup_model(self) -> None:
-        self.Q_table = np.zeros((self.observation_space.n, self.action_space.n))
+        n_observations = env.observation_space.n
+        n_actions = env.action_space.n
+        self.Q_table = np.zeros((n_observations, n_actions))
+
+        self.max_n_timesteps = 100000
+        self.max_iter_episode = 100
+        self.exploration_proba = 1
+        self.exploration_decreasing_decay = 0
+        self.min_exploration_proba = 0.01
+        self.gamma = gamma
+        self.lr = learning_rate
+
+        self.total_rewards_episode = []
+        self.n_timesteps = 0
+        self.num_episode = 0
+
+        self.current_state = None
+        self.action = None
+        self.reward = None
+        self.done = None
+        self.info = None
+        self.next_state = None
+        self.callback = None
+        self.total_episode_reward = 0
 
     def train(self):
-        current_state, action, reward, next_state, done = self.rollout
-        lr = self.learning_rate
-        gamma = self.gamma
+        self.n_timesteps += 1
 
-        q_new = (1-lr) * self.Q_table[current_state, action] +lr*(reward + gamma*max(self.Q_table[next_state,:]))
-
-        self.Q_table[current_state, action] = q_new
-
-    def learn(
-        self: SelfQ, total_timesteps: int, callback: MaybeCallback = None, log_interval: int = 1,
-        tb_log_name: str = "run", reset_num_timesteps: bool = True, progress_bar: bool = False
-    ):
-        total_timesteps, callback = self._setup_learn(
-            total_timesteps, callback, reset_num_timesteps, tb_log_name, progress_bar
-        )
-
-        total_episode_reward = 0
-
-        while self.num_timesteps < total_timesteps:
-            rollout = self.collect_rollout(callback, log_interval)
-
-            if rollout.continue_training is False:
-                break
-
-            self.train()
-
-            reward, done = self.rollout[2], self.rollout[4]
-
-            total_episode_reward += reward
-
-            if done:
-                callback.rewards.append(total_episode_reward)
-
-                total_episode_reward = 0
-
-            self.num_timesteps += 1
-
-    def collect_rollout(
-        self, callback: BaseCallback, log_interval: Optional[int] = None
-    ) -> RolloutReturn:
-        callback.on_rollout_start()
-        continue_training = True
-
-        current_state = deepcopy(self._last_obs)
-        action = self._sample_action()
-        next_state, reward, done, info = self._do_step(action)
-
-        self.rollout = current_state, action, reward, next_state, done
-
-        # Give access to local variables
-        callback.update_locals(locals())
-
-        # Only stop training if return value is False, not when it is None.
-        if callback.on_step() is False:
-            return RolloutReturn(1, 0, continue_training=False)
-
-        self._last_obs = deepcopy(next_state)
-
-        callback.on_rollout_end()
-
-        return RolloutReturn(1, 0, continue_training)
-
-    def _sample_action(self):
-        if np.random.uniform(0, 1) < 0.2:
-            action = self.action_space.sample()
+        if np.random.uniform(0,1) < 0.2:
+            self.action = self.action_space.sample()
         else:
-            action = np.argmax(self.Q_table[self._last_obs,:])
+            self.action = np.argmax(self.Q_table[self.current_state,:])
 
-        return action
+        self.next_state, self.reward, self.done, self.info = self.env.step([self.action])
+        self.next_state = self.next_state.item()
+        self.reward = self.reward.item()
+        self.done = self.done.item()
+        self.info = self.info[0]
 
-    def _do_step(self, action):
-        current_state = self._last_obs
-        new_obs, rewards, dones, infos = self.env.step([action])
-        new_ob, reward, done, info = new_obs[0], rewards[0], dones[0], infos[0]
+        if self.done:
+            self.next_state = self.info['terminal_observation']
 
-        if done:
-            new_ob = info['terminal_observation']
+        q_value = (1-self.lr) * self.Q_table[self.current_state, self.action] + self.lr*(self.reward + self.gamma*max(self.Q_table[self.next_state,:]))
+        self.Q_table[self.current_state, self.action] = q_value
+        self.total_episode_reward += self.reward
 
-        return new_ob, reward, done, info
+        self.current_state = self.next_state
 
-    def _setup_learn(
-        self, total_timesteps: int, callback: MaybeCallback = None, reset_num_timesteps: bool = True,
-        tb_log_name: str = "run", progress_bar: bool = False
-    ) -> Tuple[int, BaseCallback]:
-        _learn = super()._setup_learn(
-            total_timesteps, callback, reset_num_timesteps, tb_log_name, progress_bar
-        )
+    def learn(self, total_timesteps, callback):
+        self.callback = callback
+        self.current_state = self.env.reset().item()
 
-        self._last_obs = self._last_obs.item()
+        timesteps = 0
 
-        return _learn
+        while self.n_timesteps < total_timesteps:
+            self.train()
+        
+            callback._on_step()
+
+            timesteps += 1
+
+            # If the episode is finished, we leave the for loop
+            if self.done:
+                self.current_state = self.env.reset().item()
+                self.callback.rewards.append(self.total_episode_reward)
+                self.total_episode_reward = 0
+                self.num_episode += 1
+                timesteps = 0
 
 class CustomCallback(BaseCallback):
     def __init__(self, verbose: int = 0):
         super().__init__(verbose)
 
-        self.n_calls = 0
         self.rewards = []
 
     def _on_step(self) -> bool:
+        self.n_calls += 1
+
         if self.n_calls % 1000 == 0:
             print(self.n_calls, np.mean(self.rewards[-100:]))
-
-        return True
 
 callback = CustomCallback()
 env = gym.make('FrozenLake-v1')
 env = TimeLimit(env, 100)
-model = Q(env, learning_rate=0.1)
-model.learn(total_timesteps=100000, callback=callback)
+model = Q(env, learning_rate=0.1, gamma=0.99)
+
+model.learn(100000, callback)
+
 print(model.Q_table)
