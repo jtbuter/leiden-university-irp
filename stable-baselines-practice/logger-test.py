5696b0503f2958ca66238b1b4f3b21dd23a7cf03
Joel Buter <jtbuter@gmail.com>
2023-04-03 14:01:24 +0200

    works, to do bugfixen!

--- stable-baselines-practice/logger-test.py
+++ stable-baselines-practice/logger-test.py
@@ -22,11 +22,12 @@ n_timesteps = 0
 num_episode = 0
 
 current_state = env.reset()
+total_episode_reward = 0
+episode_length = 0
 
 #we iterate over episodes
 while n_timesteps < max_n_timesteps:
     done = False
-    total_episode_reward = 0
 
     n_timesteps += 1
 
@@ -39,12 +40,19 @@ while n_timesteps < max_n_timesteps:
 
     Q_table[current_state, action] = (1-lr) * Q_table[current_state, action] +lr*(reward + gamma*max(Q_table[next_state,:]))
     total_episode_reward += reward
+    episode_length += 1
+
+    if episode_length == 0:
+        done = True
 
     # If the episode is finished, we leave the for loop
     if done:
         total_rewards_episode.append(total_episode_reward)
         num_episode += 1
 
+        episode_length = 0
+
+        total_episode_reward = 0
         next_state = env.reset()
 
     current_state = next_state
@@ -56,3 +64,5 @@ while n_timesteps < max_n_timesteps:
 
         logger.record('reward', mean_reward)
         logger.dump(step = num_episode)
+
+print(Q_table)
