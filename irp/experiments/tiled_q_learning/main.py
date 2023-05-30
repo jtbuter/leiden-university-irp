b1af442bdcaa3116b3fc0cf2804bdce8db324160
Joel Buter <jtbuter@gmail.com>
2023-05-30 16:47:49 +0200

    met tile coding kunnen we zonder neighborhood redelijke accuracy behalen, volgende stap is met neighborhood de borders mogelijk optimaliseren

--- irp/experiments/tiled_q_learning/main.py
+++ irp/experiments/tiled_q_learning/main.py
@@ -27,21 +27,22 @@ def evaluate(environment: Tiled, policy: TiledQ, steps: int = 10, ti: Optional[i
 
     return info['d_sim']
 
-real = irp.utils.read_image(os.path.join(irp.GIT_DIR, '../data/trus/labels/case10_11.png'))
 
-s_width, s_height, overlap, n_size = 16, 8, 0.875, 0 # Define characteristics for the training and testing samples
-subimages, sublabels = irp.utils.get_subimages('case10_10.png', s_width, s_height, overlap) # Get all training instances
-t_subimages, t_sublabels = irp.utils.get_subimages('case10_11.png', s_width, s_height, overlap) # Get all training instances
+train, test = 11, 12
+
+real = irp.utils.read_image(os.path.join(irp.GIT_DIR, f'../data/trus/labels/case10_{test}.png'))
+s_width, s_height, overlap, n_size = 16, 8, 0, 0 # Define characteristics for the training and testing samples
+subimages, sublabels = irp.utils.get_subimages(f'case10_{train}.png', s_width, s_height, overlap) # Get all training instances
+t_subimages, t_sublabels = irp.utils.get_subimages(f'case10_{test}.png', s_width, s_height, overlap) # Get all training instances
 n_thresholds, tiles_per_dim, tilings, limits = 4, (2, 2, 2), 64, [(0, 1), (0, 1), (0, 32)] # Characteristics for tile-coding
 alpha = 0.2
 gamma = 0.95
 ep_frac = 0
 ep_min = 0.1
 
-coords = [(256, 176), (288, 176), (240, 184), (256, 184), (224, 192), (240, 192), (304, 192), (224, 200), (240, 200), (304, 200), (320, 200), (224, 208), (240, 208), (304, 208), (320, 208), (208, 216), (240, 216), (208, 224), (336, 224), (208, 232), (192, 240), (336, 240), (192, 248), (272, 248), (304, 248), (208, 264), (256, 264), (272, 264), (208, 272), (224, 272), (240, 272), (256, 272), (288, 272), (240, 280), (288, 280), (304, 280), (304, 288), (320, 288)]
+coords = irp.utils.extract_subimages(np.zeros((512, 512)), s_width, s_height, overlap)[1]
 
 result = np.zeros((512, 512))
-real = np.zeros((512, 512))
 
 failed = []
 train_d_sims = []
@@ -107,7 +108,6 @@ for coord in coords:
         failed.append(coord)
 
     result[y:y+s_height, x:x+s_width] = t_environment.bitmask
-    real[y:y+s_height, x:x+s_width] = t_environment.label
 
 # plt.plot(np.convolve(train_d_sims, np.ones(10) / 10, mode='same'), label='train')
 # plt.plot(np.convolve(eval_d_sims, np.ones(10) / 10, mode='same'), label='eval')
@@ -118,9 +118,9 @@ for coord in coords:
 
 # irp.utils.show(np.hstack([t_environment.label, t_environment.bitmask]))
 
-print(failed)
-print(round(1 - (len(failed) / len(coords)), 2), len(failed), len(coords))
-# print(sklearn.metrics.f1_score((real / 255).astype(bool).flatten(), (result / 255).astype(bool).flatten()))
+# print(failed)
+# print(round(1 - (len(failed) / len(coords)), 2), len(failed), len(coords))
+print(sklearn.metrics.f1_score((real / 255).astype(bool).flatten(), (result / 255).astype(bool).flatten()))
 
-# plt.imshow(np.hstack([real, result]), cmap='gray', vmin=0, vmax=1)
-# plt.show()
+plt.imshow(np.hstack([real, result]), cmap='gray', vmin=0, vmax=1)
+plt.show()
