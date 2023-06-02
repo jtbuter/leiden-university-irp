eb0a72c8e05fbcfba2f395ecfd31e658ba951462
Joel Buter <jtbuter@gmail.com>
2023-06-02 17:57:23 +0200

    halen nu redelijke accuracy

--- irp/experiments/tiled_q_learning/main.py
+++ irp/experiments/tiled_q_learning/main.py
@@ -1,5 +1,5 @@
 import os
-from typing import Optional
+from typing import List, Optional
 import matplotlib.pyplot as plt
 import numpy as np
 import sklearn.metrics
@@ -12,48 +12,45 @@ import irp.envs as envs
 import irp.utils
 import irp
 
-def evaluate(environment: Tiled, policy: TiledQ, steps: int = 10, ti: Optional[int] = None, return_done: Optional[bool] = False):
+def evaluate(environment: Tiled, policies: List[TiledQ], steps: int = 10, ti: Optional[int] = None, return_done: Optional[bool] = False):
     if ti is None:
         ti = (environment.n_thresholds - 1) * (np.random.random() > 0.5)
 
     state = environment.reset(ti=ti)
 
     for i in range(steps):
-        action = policy.predict(state)
+        actions = [policy.predict(state) for policy in policies]
+        action = irp.utils.vote(actions)
+        # action = irp.utils.vote([actions[2]])
         state, reward, done, info = environment.step(action)
 
     if return_done:
-        return np.isclose(info['d_sim'], environment.d_sim)
+        return done
 
     return info['d_sim']
 
 train, test = 11, 12
 
 real = irp.utils.read_image(os.path.join(irp.GIT_DIR, f'../data/trus/labels/case10_{test}.png'))
-s_width, s_height, overlap, n_size = 16, 8, 0.75, 1 # Define characteristics for the training and testing samples
+s_width, s_height, overlap, n_size = 16, 8, 0, 0 # Define characteristics for the training and testing samples
 subimages, sublabels = irp.utils.get_subimages(f'case10_{train}.png', s_width, s_height, overlap) # Get all training instances
 t_subimages, t_sublabels = irp.utils.get_subimages(f'case10_{test}.png', s_width, s_height, overlap) # Get all training instances
 n_thresholds, tiles_per_dim, tilings, limits = 4, (2, 2, 2), 64, [(0, 1), (0, 1), (0, 32)] # Characteristics for tile-coding
-alpha = 0.6
-gamma = 0.6
-ep_frac = 0.001
-ep_max = 0.7
-ep_min = 0.7
+alpha = 0.8
+gamma = 0.9
+ep_frac = 0.00025
+ep_max = 0.3
+ep_min = 0.3
 
-coords = irp.utils.extract_subimages(np.zeros((512, 512)), s_width, s_height, 0)[1]
-# coords = [(224, 240)]
+coords = irp.utils.extract_subimages(np.zeros((512, 512)), s_width, s_height, overlap=0)[1]
+# coords = [(208, 240)]
 
 result = np.zeros((512, 512))
-
-failed = []
-train_d_sims = []
-eval_d_sims = []
-ts = []
-exploit = 0
 
 for coord in coords:
     x, y = coord
     index = irp.utils.coord_to_id(coord, (512, 512), s_width, s_height, overlap)
+    policies = []
 
     if not (x >= 192 and x <= 336 and y >= 176 and y <= 288):
         continue
@@ -63,76 +60,82 @@ for coord in coords:
     t_environment = Tiled(t_environment, tiles_per_dim, tilings, limits)
 
     samples, labels = irp.utils.get_neighborhood_images(subimages, sublabels, coord, s_width, s_height, overlap, n_size, (512, 512), 'neumann')
-    environments = wrappers.MultiSample([])
 
     for sample, label in zip(samples, labels):
+        failed = []
+        train_d_sims = []
+        eval_d_sims = []
+        ts = []
+        exploit = 0
+
         environment = Env(sample, label, n_thresholds=n_thresholds)
         environment = Tiled(environment, tiles_per_dim, tilings, limits)
-        environments.add(environment)
+        
+        policy = TiledQ(environment.T.n_tiles, environment.action_space.n, alpha)
+        
+        t = 0
+        ep = ep_max
 
-    environment = environments
+        while t < 15000: # Perform `n` total timesteps
+            state = environment.reset()
+            if np.random.random() < ep: action = environment.action_space.sample()
+            else: action = policy.predict(state)
 
-    policy = TiledQ(environment.T.n_tiles, environment.action_space.n, alpha)
-    
-    t = 0
-    ep = ep_max
+            for _ in range(30): # Perform `k` timesteps
+                next_state, reward, done, info = environment.step(action)
 
-    while t < 15000: # Perform `n` total timesteps
-        state = environment.reset()
-        if np.random.random() < ep: action = environment.action_space.sample()
-        else: action = policy.predict(state)
+                if np.random.random() < ep: next_action = environment.action_space.sample()
+                else: next_action = policy.predict(state)
+                # next_action = policy.predict(state)
 
-        for _ in range(30): # Perform `k` timesteps
-            next_state, reward, done, info = environment.step(action)
+                target = reward + gamma * policy.value(next_state, next_action)
 
-            if np.random.random() < ep: next_action = environment.action_space.sample()
-            else: next_action = policy.predict(state)
-            # next_action = policy.predict(state)
+                policy.update(state, action, target)
 
-            target = reward + gamma * policy.value(next_state, next_action)
+                state = next_state
+                action = next_action
 
-            policy.update(state, action, target)
+                t += 1
 
-            state = next_state
-            action = next_action
+                ep = max(ep_min, ep - ep_frac)
 
-            t += 1
+                if exploit <= 0 and ep == ep_min:
+                    exploit = t
 
-            ep = max(ep_min, ep - ep_frac)
+            # ts.append(t)
+            # train_d_sims.append(evaluate(environment, policy))
+            # eval_d_sims.append(evaluate(t_environment, policy, ti=0))
 
-            if exploit <= 0 and ep == ep_min:
-                exploit = t
+        policies.append(policy)
 
-        if len(coords) < 10:
-            ts.append(t)
-            train_d_sims.append(evaluate(environment, policy, return_done=True))
-            eval_d_sims.append(evaluate(t_environment, policy, ti=0))
+    # dissim, seq = irp.utils.get_best_dissimilarity(
+    #     t_environment.sample, t_environment.label, [t_environment.intensity_spectrum], [envs.utils.apply_threshold], return_seq=True
+    # )
 
-    d_sim = evaluate(t_environment, policy, ti=0)
+    # print(dissim, seq, np.nonzero(t_environment.intensity_spectrum == seq[0])[0][0])
+
+    # print(evaluate(t_environment, policies, ti=0))
+
+    d_sim = evaluate(t_environment, policies, ti=0)
 
     print(coord, d_sim, t_environment.d_sim)
 
-    if not np.isclose(d_sim, t_environment.d_sim):
-        failed.append(coord)
+        # if not np.isclose(d_sim, t_environment.d_sim):
+        #     failed.append(coord)
 
     result[y:y+s_height, x:x+s_width] = t_environment.bitmask
 
-if len(coords) < 10:
-    print(exploit)
+        # print(exploit)
 
-    plt.plot(ts, np.convolve(train_d_sims, np.ones(10) / 10, mode='same'), label='train')
-    plt.plot(ts, np.convolve(eval_d_sims, np.ones(10) / 10, mode='same'), label='eval')
-    plt.axhline(t_environment.d_sim, linestyle='--', color='red', label='best test d_sim')
-    plt.axhline(environment.d_sim, linestyle='--', color='orange', label='best train d_sim')
-    plt.axvline(exploit, linestyle='--', color='grey', label='exploitation')
-    plt.legend()
-    plt.show()
+        # plt.plot(ts, np.convolve(train_d_sims, np.ones(10) / 10, mode='same'), label='train')
+        # plt.plot(ts, np.convolve(eval_d_sims, np.ones(10) / 10, mode='same'), label='eval')
+        # plt.axhline(t_environment.d_sim, linestyle='--', color='red', label='best test d_sim')
+        # plt.axhline(environment.d_sim, linestyle='--', color='orange', label='best train d_sim')
+        # # plt.axvline(min(exploit, len(train_d_sims)), linestyle='--', color='grey', label='exploitation')
+        # plt.legend()
+        # plt.show()
 
-# irp.utils.show(np.hstack([t_environment.label, t_environment.bitmask]))
-
-# print(failed)
-# print(round(1 - (len(failed) / len(coords)), 2), len(failed), len(coords))
-print(sklearn.metrics.f1_score(real.astype(bool).flatten(), result.astype(bool).flatten()))
+print(round(sklearn.metrics.f1_score(real.astype(bool).flatten(), result.astype(bool).flatten()), 2))
 
 plt.imshow(np.hstack([real, result]), cmap='gray', vmin=0, vmax=1)
 plt.show()
